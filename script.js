80bd4b2764a1b6a501327bfeab6ea4cc4815b39b
Sameer069 <112536688+Sameer069@users.noreply.github.com>
2023-12-30 13:18:36 +0530

    upload via

---
+++ script.js
@@ -0,0 +1,18 @@
+// console.log("hello world");
+let num=[1,2,3,8,9,67];
+// num.sort();
+// console.log(num)
+// let com=(a,b)=>{
+// return a-b;
+// }
+// num.sort(com);
+// console.log(num);
+// num.splice(3);
+// console.log(num);
+
+num.splice(1,3,67,89,90,54,78);
+console.log(num);
+
+
+// let num1=num.slice(2,5);
+// console.log(num1);
